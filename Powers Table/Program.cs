20ae4929d3443240f9fe9a5ffef201e19d053f03
Kyle Vaughn <kcvaughn75@gmail.com>
2020-01-24 08:53:05 -0500

    second entry for Powers Table

--- Powers Table/Program.cs
+++ Powers Table/Program.cs
@@ -10,20 +10,22 @@ namespace ConsoleApplication1
             int Square, Cube;
             char userResponse = 'y';
 
+            //while loop allows program to run again as long as the user inputs "y".
             while (userResponse == 'y')
             {
                 Console.WriteLine("Please enter an integer.");
                 int userNumber = int.Parse(Console.ReadLine());
 
                 Console.WriteLine("Number\tSquare\tCube\n");
-                //loop 10 times calculating and displaying values
+                
+                //calculates squares and cubes from the integer 1 through whatever number the user chooses
                 for (int Number = 1; Number <= userNumber;
                Number++)
                 {
                     Square = Number * Number;
                     Cube = Square * Number;
                     Console.WriteLine("{0}\t{1}\t{2}", Number, Square, Cube);
-                }// end of calculate and display
+                }
 
                 Console.WriteLine("Would you like to continue? [y/n}");
                 userResponse = char.Parse(Console.ReadLine());
